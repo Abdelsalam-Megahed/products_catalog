78b06a751dc16f0ddfaacc03298f90dfc187718e
abdelsalam.megahed <abdelsalam.megahed@gmail.com>
2020-04-14 13:40:22 +0300

    Error handling is improved.

--- products_catalog/app/Http/Controllers/ProductController.php
+++ products_catalog/app/Http/Controllers/ProductController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Product;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
@@ -15,28 +18,70 @@ class ProductController extends Controller
 
     public function show($id)
     {
+        $product = Product::find($id);
+        if(is_null($product)){
+            return response()->json(['error' => 'Record not found'], Response::HTTP_NOT_FOUND);
+        }
         return Product::find($id);
+    }
+
+    public function validateRequest(Request $request){
+        $rules = [
+            'name' => 'required|min:3',
+            'description' => 'required|min:20',
+            'price','quantity', 'rating' => 'required|Numeric',
+            'in_stock' => 'required|boolean',
+            'category_id' =>'required|integer'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        return $validator;
     }
 
     public function store(Request $request)
     {
+        $rules = [
+            'name' => 'required|min:3',
+            'description' => 'required|min:20',
+            'price','rating' => 'required|Numeric',
+            'quantity' => 'required|integer',
+            'in_stock' => 'required|boolean',
+            'category_id' =>'required|integer'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()){
+            return response()->json(["error" => $validator->errors()], Response::HTTP_BAD_REQUEST);
+        }
+
         $product = Product::create($request->all());
-        return response()->json([$product , Response::HTTP_CREATED]);
+        return response()->json([$product], Response::HTTP_CREATED);
     }
 
     public function update(Request $request, $id)
     {
+        $rules = [
+            'name' => 'min:3',
+            'description' => 'min:20',
+            'price','rating' => 'Numeric',
+            'quantity' => 'integer',
+            'in_stock' => 'boolean',
+            'category_id' =>'integer'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()){
+            return response()->json(["error" => $validator->errors()], Response::HTTP_BAD_REQUEST);
+        }
+
         $product = Product::findOrFail($id);
         $product->update($request->all());
-
         return $product;
     }
 
-    public function delete(Request $request, $id)
+    public function delete($id)
     {
         $product = Product::findOrFail($id);
         $product->delete();
 
-        return response()->json(["status_code", Response::HTTP_NO_CONTENT]);
+        return response()->json(["response" => "Product has been deleted"], Response::HTTP_ACCEPTED);
     }
 }
