d4ca9a32b47fd8adbfef2cb5b6c48f30e5ca813a
abdelsalam.megahed <abdelsalam.megahed@gmail.com>
2020-04-14 14:42:00 +0300

    Revert "Error handling is improved."
    This reverts commit 78b06a751dc16f0ddfaacc03298f90dfc187718e.

--- products_catalog/app/Http/Controllers/CategoryController.php
+++ products_catalog/app/Http/Controllers/CategoryController.php
@@ -2,62 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 use App\Category;
 
 class CategoryController extends Controller
 {
-
     public function index()
     {
-        $categories = Category::with('products')->get();
-        return $categories;
+        return Category::all();
     }
 
     public function show($id)
     {
         $category = Category::find($id);
-        if(is_null($category)){
-            return response()->json(['error' => 'Record not found'], Response::HTTP_NOT_FOUND);
-        }
         $category->products;
         return $category;
     }
 
     public function store(Request $request)
     {
-        $validator =  $this->validateRequest($request);
-        if($validator->fails()){
-            return response()->json(["error" => $validator->errors()], Response::HTTP_BAD_REQUEST);
-        }
-
         $category = Category::create($request->all());
         return response()->json([$category , Response::HTTP_CREATED]);
     }
 
     public function update(Request $request, $id)
-    {   $validator =  $this->validateRequest($request);
-        if($validator->fails()){
-            return response()->json(["error" => $validator->errors()], Response::HTTP_BAD_REQUEST);
-        }
-
+    {
         $category = Category::findOrFail($id);
         $category->update($request->all());
+
         return $category;
     }
 
-    public function delete($id)
+    public function delete(Request $request, $id)
     {
         $category = Category::findOrFail($id);
         $count = count($category->products);
 
         if($count == 0){
             $category->delete();
-            return response()->json(["response" => "Category has been deleted"], Response::HTTP_ACCEPTED);
+            return response()->json(["status_code" => Response::HTTP_NO_CONTENT]);
         }
         else{
             return response()->json([
@@ -66,14 +51,5 @@ class CategoryController extends Controller
                 Response::HTTP_FORBIDDEN);
         }
 
-    }
-
-    public function validateRequest(Request $request){
-        $rules = [
-            'name' => 'required|min:3|unique:categories',
-        ];
-
-        $validator = Validator::make($request->all(), $rules);
-        return $validator;
     }
 }
